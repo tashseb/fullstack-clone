b3786ce11f161383e972bfd74cb688a06646946f
Tash Cruz <sebastiantashika@gmail.com>
2022-10-04 17:24:43 +0900

    saving other changes made

--- 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
+++ 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
@@ -1,4 +1,12 @@
 def colorful?(number)
   # TODO: return true if the number is colorful, false otherwise
-end
+  num = number.to_s.scan(/./).map { |e| e.to_i }
 
+  if num.length <= 3
+    r = color[0] * color[1]
+    g = color[1] * color[-1]
+    b = r * color[-1]
+  else
+    return false
+  end
+end
