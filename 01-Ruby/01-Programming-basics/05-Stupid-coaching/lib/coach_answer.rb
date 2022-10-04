196d751612047bdf549acb3abb3ed4395fa857dc
Tash Cruz <sebastiantashika@gmail.com>
2022-10-04 13:52:58 +0900

    made the code prettier?

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -12,14 +12,14 @@ end
 def coach_answer_enhanced(your_message)
   # TODO: return coach answer to your_message, with additional custom rules of yours!
   if your_message == "I WOULD LOVE SOME PIZZA!"
-      "I can feel your motivation! I don't care, get dressed and go to work!"
+    "I can feel your motivation! I don't care, get dressed and go to work!"
   elsif your_message == "CAN I EAT SOME PIZZA?"
-      "I can feel your motivation! Silly question, get dressed and go to work!"
-  elsif your_message == "I would love to eat some pizza!"
-    "I don't care, get dressed and go to work!"
-  elsif your_message.end_with?("?")
-    "Silly question, get dressed and go to work!"
+    "I can feel your motivation! Silly question, get dressed and go to work!"
+  #elsif your_message == "I would love to eat some pizza!"
+   # "I don't care, get dressed and go to work!"
+  #elsif your_message.end_with?("?")
+    #"Silly question, get dressed and go to work!"
   else
-    ""
+    coach_answer(your_message)
   end
 end
