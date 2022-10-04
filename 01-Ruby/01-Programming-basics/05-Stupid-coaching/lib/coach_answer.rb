fe28f1f1c05c58c065ba17d37f2e14257b75a228
Tash Cruz <sebastiantashika@gmail.com>
2022-10-04 13:46:46 +0900

    added codes on coach_answer and interface

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,8 +1,25 @@
 def coach_answer(your_message)
   # TODO: return coach answer to your_message
+  if your_message.end_with?("?")
+    "Silly question, get dressed and go to work!"
+  elsif your_message == "I would love to eat some pizza!"
+    "I don't care, get dressed and go to work!"
+  else
+    ""
+  end
 end
 
 def coach_answer_enhanced(your_message)
   # TODO: return coach answer to your_message, with additional custom rules of yours!
+  if your_message == "I WOULD LOVE SOME PIZZA!"
+      "I can feel your motivation! I don't care, get dressed and go to work!"
+  elsif your_message == "CAN I EAT SOME PIZZA?"
+      "I can feel your motivation! Silly question, get dressed and go to work!"
+  elsif your_message == "I would love to eat some pizza!"
+    "I don't care, get dressed and go to work!"
+  elsif your_message.end_with?("?")
+    "Silly question, get dressed and go to work!"
+  else
+    ""
+  end
 end
-
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,6 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+puts "What do you want to tell your coach?"
+feedback = gets.chomp
+
+puts coach_answer(feedback) or coach_answer_enhanced(feedback)
