a9ba0a67fd669a0256045ce1f6217858dbc9fac7
Tash Cruz <sebastiantashika@gmail.com>
2022-10-04 23:40:55 +0900

    trying to code fizz_buzz

--- 01-Ruby/02-Flow-Array/Optional-04-Fizz-Buzz/lib/fizz_buzz.rb
+++ 01-Ruby/02-Flow-Array/Optional-04-Fizz-Buzz/lib/fizz_buzz.rb
@@ -1,3 +1,19 @@
 def fizz_buzz(number)
   # TODO: return an array of integers, 'Fizz', 'Buzz' or 'FizzBuzz'
+  result = []
+  i = 1
+  @number = number
+
+  while i <= number
+    text = ""
+    text += "Fizz" if i % 3.zero?
+    text += "Buzz" if i % 5.zero?
+    text = i if text.empty?
+
+    result.push(text)
+    i += 1
+  end
+  raise ArgumentError, "Argument must be greater than one" unless @number > 1
+
+  return result
 end
