7d7cba2b3e0841f644e7f33199714edbf6935598
Tash Cruz <sebastiantashika@gmail.com>
2022-10-05 11:07:45 +0900

    made it prettier

--- 01-Ruby/02-Flow-Array/01-Can-you-vote/lib/vote.rb
+++ 01-Ruby/02-Flow-Array/01-Can-you-vote/lib/vote.rb
@@ -1,9 +1,5 @@
 def allowed_to_vote?(age)
   # TODO: return (not print!) a boolean stating whether `age` is old enough to vote
   # NOTE: Use an if/else statment
-  if age >= 18
-    true
-  else
-    false
-  end
+  age >= 18
 end
