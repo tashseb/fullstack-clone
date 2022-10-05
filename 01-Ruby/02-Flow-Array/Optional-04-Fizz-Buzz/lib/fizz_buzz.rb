6e8db45fef05dd23228a8cf1d7ee10866eae8db9
Tash Cruz <sebastiantashika@gmail.com>
2022-10-05 16:54:18 +0900

    removed all errors. Working code. Rubo cop is happy

--- 01-Ruby/02-Flow-Array/Optional-04-Fizz-Buzz/lib/fizz_buzz.rb
+++ 01-Ruby/02-Flow-Array/Optional-04-Fizz-Buzz/lib/fizz_buzz.rb
@@ -1,13 +1,17 @@
 def fizz_buzz(number)
   # TODO: return an array of integers, 'Fizz', 'Buzz' or 'FizzBuzz'
+  # returns an array of one if 'number' is one
+
+  return [1] if number == 1
+  raise ArgumentError, "Argument must be greater than one" unless number > 1
+
   result = []
   i = 1
-  @number = number
 
   while i <= number
     text = ""
-    text += "Fizz" if i % 3 == 0
-    text += "Buzz" if i % 5 == 0
+    text += "Fizz" if (i % 3).zero?
+    text += "Buzz" if (i % 5).zero?
     if text.empty?
       result << i
     else
@@ -15,8 +19,5 @@ def fizz_buzz(number)
     end
     i += 1
   end
-
-  raise ArgumentError, "Argument must be greater than one" unless @number > 1
-
   return result
 end
