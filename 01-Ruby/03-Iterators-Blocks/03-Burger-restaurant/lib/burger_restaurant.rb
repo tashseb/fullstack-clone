84dd8103d089e290c181fcf7acc41a69144daa93
Tash Cruz <sebastiantashika@gmail.com>
2022-10-06 13:53:32 +0900

    added some codes

--- 01-Ruby/03-Iterators-Blocks/03-Burger-restaurant/lib/burger_restaurant.rb
+++ 01-Ruby/03-Iterators-Blocks/03-Burger-restaurant/lib/burger_restaurant.rb
@@ -1,3 +1,15 @@
 def burger(patty, sauce, topping)
   # TODO: code the `burger` method
+  finished_burger = []
+  finished_burger[0] = "bread"
+  finished_burger[1] = patty
+  finished_burger[2] = sauce
+  finished_burger[3] = topping
+  finished_burger[4] = "bread"
+  yield(patty, sauce, topping) if block_given?
+  finished_burger
+end
+
+burger('steak', 'ketchup', 'onions') do |patty|
+  "grilled #{patty}"
 end
--- 01-Ruby/03-Iterators-Blocks/03-Burger-restaurant/lib/interface.rb
+++ 01-Ruby/03-Iterators-Blocks/03-Burger-restaurant/lib/interface.rb
@@ -1,13 +1,25 @@
 require_relative "burger_restaurant"
 
+
+puts "Please choose a patty: steak, chicken and fish"
+patty = gets.chomp
+
+puts "Please select a sauce:"
+sauce = gets.chomp
+
+puts "Please select a topping:"
+topping = gets.chomp
+
+burger(patty, sauce, topping)
+
 puts "💬 I'd like a burger with a bigger portion of fish, plus mayo and salad please."
 # TODO: to upgrade a portion to a bigger one, transform the fish to uppercase
-# bigger_burger =
+bigger_burger = burger(patty.upcase, sauce, topping)
 
 
 puts "💬 I'd like a juicy steak with barbecue sauce and cheddar please."
 # TODO: to make a juicy steak, replace any vowel by the sign "~"
-# juicy_burger =
+juicy_burger = burger(patty, sauce, topping)
 
 
 puts "💬 I'd like a spicy chicken with ketchup and cheddar please."
