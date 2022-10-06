8862d58d49142feed487e27207da2330335c0d4f
Tash Cruz <sebastiantashika@gmail.com>
2022-10-06 11:34:48 +0900

    added working code

--- 01-Ruby/03-Iterators-Blocks/02-Enumerable/lib/enumerable.rb
+++ 01-Ruby/03-Iterators-Blocks/02-Enumerable/lib/enumerable.rb
@@ -1,34 +1,40 @@
 def sum_odd_indexed(array)
-  # TODO: computes the sum of elements at odd indexes (1, 3, 5, 7, etc.)
-  #       You should use Enumerable#each_with_index
+  sum = 0
+  array.each_with_index { |item, index| sum += item if index.odd? }
+  sum
 end
 
 def even_numbers(array)
-  # TODO: Return the even numbers from a list of integers.
-  #       You should use Enumerable#select
+  array.select { |num| num.even? }
 end
 
 def short_words(array, max_length)
   # TODO: Take an array of words, return the array of words not exceeding max_length characters
   #       You should use Enumerable#reject
+  array.reject { |word| word if word.size > max_length }
 end
 
 def first_under(array, limit)
   # TODO: Return the first number from an array that is less than limit.
   #       You should use Enumerable#find
+  array.find { |number| number if number < limit }
 end
 
 def add_bang(array)
-  # TODO: Take an array of strings and return a new array with "!" appended to each string.
-  #       You should use Enumerable#map
+  array.map { |string| "#{string}!" }
 end
 
 def concatenate(array)
-  # TODO: Concatenate all strings given in the array.
-  #       You should use Enumerable#reduce
+  # ["hello", " ", "all", ",", " ", "how", " ", "are", " ", "you", "?"]
+  # "hello all, how are you?" empty elements
+  array.reduce { |memo, obj| memo if obj != " " }
+  array.join
 end
 
 def sorted_pairs(array)
-  # TODO: Reorganize an array into slices of 2 elements, and sort each slice alphabetically.
-  #       You should use Enumerable#each_slice
+  # ["say" "my" "name" "say" "my" "name"]
+  # [["my", "say"],["name", "say"], ["my", "name"]]
+  result = []
+  array.each_slice(2) { |value| result << value.sort }
+  result
 end
