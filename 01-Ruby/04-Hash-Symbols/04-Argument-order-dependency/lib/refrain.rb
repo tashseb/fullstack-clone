8ec1a1237f7fa1a470fb70efc06c08a471e931c9
Tash Cruz <sebastiantashika@gmail.com>
2022-10-07 15:18:10 +0900

    prettier

--- 01-Ruby/04-Hash-Symbols/04-Argument-order-dependency/lib/refrain.rb
+++ 01-Ruby/04-Hash-Symbols/04-Argument-order-dependency/lib/refrain.rb
@@ -12,14 +12,10 @@ end
 
 def better_refrain(lyrics, options = { vibrato: 0, number_of_times: 1, strong: false })
   song_refrain = []
-  if options.empty?
-    lyrics
-  else
-    lyrics.upcase! if options[:strong]
-    lyrics += lyrics[lyrics.size - 1] * options[:vibrato]
-    options[:number_of_times].times do
-      song_refrain << lyrics
-    end
-  end
+
+  lyrics.upcase! if options[:strong]
+  lyrics += lyrics[lyrics.size - 1] * options[:vibrato]
+  options[:number_of_times].times { song_refrain << lyrics }
+
   song_refrain.join(" ")
 end
