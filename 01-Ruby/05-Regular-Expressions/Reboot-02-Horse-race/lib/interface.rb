a25647a69c82d18905454247871446aead9b052f
Tash Cruz <sebastiantashika@gmail.com>
2022-10-10 13:17:53 +0900

    working code

--- 01-Ruby/05-Regular-Expressions/Reboot-02-Horse-race/lib/interface.rb
+++ 01-Ruby/05-Regular-Expressions/Reboot-02-Horse-race/lib/interface.rb
@@ -1,21 +1,29 @@
 # TODO: you can build your horse race program here!
 horses = ['lucky coder', 'big time', 'fast rabbit', 'crazy diamond', 'sho']
 
-result = nil
-until result == "N"
+result = "Y"
+balance = 100
+until result =~ /n/i
   winner = horses.sample
   horses.each_with_index { |horse, index| puts "#{index + 1}. #{horse}" }
   puts "Please select the number of your chosen horse:"
   choice = gets.chomp.to_i - 1
   # choice = get the user's choice (convert to integer then convert index) (- 1 for index)
   # choice_horse = convert the user's number into a horse's name
-  choice_horse = horses.find.with_index { |_horse, index| index == choice }
-
+  choice_horse = horses[choice]
+  puts "Winning horse is #{winner}"
   if choice_horse == winner
-    puts "Congratulations! Winning horse is #{winner} You chose: #{choice_horse}"
+    balance += 50
+    puts "You win You chose: #{choice_horse}"
   else
-    puts "Sorry. You lose. You chose: #{choice_horse}. Winning horse is #{winner}"
+    balance -= 10
+    puts "Sorry, you lose. You chose: #{choice_horse}"
   end
-  puts "Want to bet on another horse? [Y/N]"
-  result = gets.chomp
+  if balance.zero?
+    puts "Sorry, you don't have enough balance to make another bet!"
+  else
+    puts "Your balance is #{balance}"
+    puts "Want to bet on another horse? [Y/N]"
+    result = gets.chomp
+  end
 end
