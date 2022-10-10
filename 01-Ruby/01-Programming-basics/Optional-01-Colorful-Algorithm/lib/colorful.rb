1a6f0b8004d17eabee6c315c324c164622619305
Tash Cruz <sebastiantashika@gmail.com>
2022-10-10 10:41:40 +0900

    some progress with the expected result

--- 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
+++ 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
@@ -1,12 +1,17 @@
 def colorful?(number)
-  # TODO: return true if the number is colorful, false otherwise
-  num = number.to_s.scan(/./).map { |e| e.to_i }
-
-  if num.length <= 3
-    r = color[0] * color[1]
-    g = color[1] * color[-1]
-    b = r * color[-1]
+  product = []
+  if number.instance_of? Integer
+    nums = number.to_s.chars.map { |numero| numero.to_i }
+    if nums.length == 1 || nums == 23
+      result true
+    else
+      nums.map.with_index do |num, index|
+        index == 2 ? product << (num * nums[0] * nums[1]) : product << (num * nums[index + 1])
+        index - 1
+      end
+      nums & product == []
+    end
   else
-    return false
+    false
   end
 end
