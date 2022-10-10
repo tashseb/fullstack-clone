ddecf9c0771f876d8e43a86cc13246bb125d499d
Tash Cruz <sebastiantashika@gmail.com>
2022-10-10 12:38:47 +0900

    added some code

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,3 +1,8 @@
 def french_phone_number?(phone_number)
   # TODO: true or false?
+  # Input is string; Returns true when phone is valid
+  # method should IGNORE spaces or dashes
+  # True -> starts with 0 and 10 digits
+  # True -> start with +33 and 11 digit
+  # Second digit cannot be zero
 end
