c830541ff8deac97c8eff88d8e3cc69d02870560
Tash Cruz <sebastiantashika@gmail.com>
2022-10-12 16:05:54 +0900

    working code

--- 02-OOP/01-OO-Basics/04-Public-Interface/lib/bank_account.rb
+++ 02-OOP/01-OO-Basics/04-Public-Interface/lib/bank_account.rb
@@ -12,6 +12,7 @@ class BankAccount
   # - You can see the balance of the account (through the balance variable)
 
   MIN_DEPOSIT = 100
+  attr_reader :name, :balance
 
   def initialize(name, iban, initial_deposit, password)
     fail DepositError, "Insufficient deposit" unless initial_deposit >= MIN_DEPOSIT
@@ -27,32 +28,51 @@ class BankAccount
 
   def withdraw(amount)
     # TODO: Call add_transaction with the right argument
+    add_transaction(-amount)
     # TODO: returns a string with a message
+    "You have withdrawn $#{amount}"
   end
 
   def deposit(amount)
     # TODO: Call add_transaction with the right argument
+    add_transaction(amount)
     # TODO: returns a string with a message
+    "You have deposited $#{amount}"
   end
 
   def transactions_history(args = {})
     # TODO: Check if there is a password and if so if it is correct
+    password = args[:password]
+    if @password == password
+      @transactions.join(" ")
+    elsif password.nil?
+      "no password given"
+      # p "@password #{@password} password #{password}"
+    elsif password != @password
+      "wrong password"
+    end
     # TODO: return a string displaying the transactions, BUT NOT return the transaction array!
   end
 
   def iban
     # TODO: Hide the middle of the IBAN like FR14**************606 and return it
+    prefix = @iban[0, 4]
+    suffix = @iban[30, 32]
+    @iban = "#{prefix}***********************#{suffix}"
   end
 
   def to_s
     # Method used when printing account object as string (also used for string interpolation)
     # TODO: return a string with the account owner, the hidden iban and the balance of the account
+    "#{@name} #{iban} #{@balance}"
   end
 
   private
 
   def add_transaction(amount)
     # TODO: add the amount in the transactions array
+    @transactions << amount
     # TODO: update the current balance (which represents the balance of the account)
+    @balance += amount
   end
 end
