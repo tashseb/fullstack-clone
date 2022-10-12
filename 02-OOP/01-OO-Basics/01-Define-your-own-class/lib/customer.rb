448da18896ff04a93a9c2f57ad7e4b5532fed258
Tash Cruz <sebastiantashika@gmail.com>
2022-10-12 11:10:02 +0900

    made Rubo Cop happy

--- 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/customer.rb
+++ 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/customer.rb
@@ -1,22 +1,19 @@
-
 class Customer
-  @@no_of_customers = 0
-
+  @no_of_customers = 0
   def initialize(id, name, age)
-    @@no_of_customers += 1
+    @no_of_customers += 1
     @cust_id = id
     @cust_name = name
     @cust_age = age
   end
 
-  def display_info()
-    puts "Customer id: #@cust_id"
-    puts "Customer id: #@cust_name"
-    puts "Customer id: #@cust_age"
+  def display_info
+    puts "Customer id: #{@cust_id}"
+    puts "Customer id: #{@cust_name}"
+    puts "Customer id: #{@cust_age}"
   end
 
-  def total_no_customers()
-    puts "Total number of customers: #@@no_of_customers"
+  def total_no_customers
+    puts "Total number of customers: #{@no_of_customers}"
   end
-
 end
