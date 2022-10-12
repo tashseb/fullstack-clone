e83e548ee8d59473bf7b5a6f7019d9c18d29319a
Tash Cruz <sebastiantashika@gmail.com>
2022-10-12 09:01:48 +0900

    working code

---
+++ 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/customer.rb
@@ -0,0 +1,22 @@
+
+class Customer
+  @@no_of_customers = 0
+
+  def initialize(id, name, age)
+    @@no_of_customers += 1
+    @cust_id = id
+    @cust_name = name
+    @cust_age = age
+  end
+
+  def display_info()
+    puts "Customer id: #@cust_id"
+    puts "Customer id: #@cust_name"
+    puts "Customer id: #@cust_age"
+  end
+
+  def total_no_customers()
+    puts "Total number of customers: #@@no_of_customers"
+  end
+
+end
---
+++ 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/interface.rb
@@ -0,0 +1,7 @@
+require_relative 'customer.rb'
+
+cust1 = Customer.new(1, "John Snow", 26)
+cust2 = Customer.new(2, "Daenarys Targaryean", 23)
+
+cust1.display_info()
+cust2.display_info()
