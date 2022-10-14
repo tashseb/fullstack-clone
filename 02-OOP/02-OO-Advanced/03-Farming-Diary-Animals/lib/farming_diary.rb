441a545923e7545c9a09fa0a32b59c00775b1d46
Tash Cruz <sebastiantashika@gmail.com>
2022-10-14 11:44:06 +0900

    some small changes

--- 02-OOP/02-OO-Advanced/03-Farming-Diary-Animals/lib/farming_diary.rb
+++ 02-OOP/02-OO-Advanced/03-Farming-Diary-Animals/lib/farming_diary.rb
@@ -36,11 +36,4 @@ Board.new.display
 
 
 
-class Customer
-  attr_reader :name, :age, :gender
-  def initialize(name, age, gender)
-    @name = name
-    @age = age
-    @gender = gender
-  end
-end
+
