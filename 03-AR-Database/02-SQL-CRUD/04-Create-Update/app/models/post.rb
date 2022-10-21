80ab5fa540969ab9c352151022736e9f6a260d0d
Tash Cruz <sebastiantashika@gmail.com>
2022-10-21 12:18:25 +0900

    all green

--- 03-AR-Database/02-SQL-CRUD/04-Create-Update/app/models/post.rb
+++ 03-AR-Database/02-SQL-CRUD/04-Create-Update/app/models/post.rb
@@ -7,7 +7,26 @@ class Post
     @url = attributes[:url]
     @votes = attributes[:votes] || 0
     @title = attributes[:title]
+    DB.results_as_hash = true
   end
 
-  # TODO
+  def save
+    if @id.nil?
+      add_post
+    else
+      update_post
+    end
+  end
+
+  def add_post
+    query = "INSERT INTO posts (url, votes, title) VALUES (?, ?, ?)"
+    DB.execute(query, @url, @votes, @title)
+    puts "Record saved!"
+    @id = DB.last_insert_row_id
+  end
+
+  def update_post
+    query = "UPDATE posts SET url = ?, votes = ?, title = ? WHERE id = ? "
+    updated_post = DB.execute(query, @url, @votes, @title, @id)
+  end
 end
