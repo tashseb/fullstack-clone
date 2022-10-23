f38b2b2b35f7d27fbd628d45356a4d8a3b41654a
Tash Cruz <sebastiantashika@gmail.com>
2022-10-23 23:36:38 +0900

    made some changes

--- 03-AR-Database/02-SQL-CRUD/05-MVC-HackerNews/app/views/posts_view.rb
+++ 03-AR-Database/02-SQL-CRUD/05-MVC-HackerNews/app/views/posts_view.rb
@@ -2,11 +2,13 @@ class PostsView
   # TODO: implement methods called by the PostsController
   def display_posts(posts)
     posts.each_with_index do |post, index|
+      puts ""
       puts "#{index + 1}.) Title: #{post.title} URL: #{post.url}, Votes: #{post.votes}"
     end
   end
 
   def add_(something)
+    puts ""
     puts "What is the #{something}?"
     print ">"
     gets.chomp
