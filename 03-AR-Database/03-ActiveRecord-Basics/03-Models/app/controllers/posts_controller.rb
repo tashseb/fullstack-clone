57e4b6c2f74ddb86f229082397571c91ce3aa82a
Tash Cruz <sebastiantashika@gmail.com>
2022-10-24 12:11:33 +0900

    all green

--- 03-AR-Database/03-ActiveRecord-Basics/03-Models/app/controllers/posts_controller.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/03-Models/app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 require_relative "../views/posts_view"
+require_relative "../models/post"
 
-class PostsController
+class PostsController < ActiveRecord::Relation
   def initialize
     @view = PostsView.new
   end
@@ -8,26 +9,45 @@ class PostsController
   def index
     # DO NOT WRITE SQL QUERIES
     # TODO: gather all posts from the database
+    posts = Post.all
+    posts.to_a.map(&:serializable_hash)
     # TODO: give them to the view to be printed
+    @view.display_posts(posts)
   end
 
   def create
     # DO NOT WRITE SQL QUERIES
     # TODO: create a post
+    title = @view.add_("title of your post.")
+    url = @view.add_("url of your post.")
+    post = Post.new(title: title, url: url)
+    post.save
   end
 
   def update
     # DO NOT WRITE SQL QUERIES
     # TODO: update a post
+    index
+    post_id = @view.add_("ID of the post you want to EDIT").to_i
+    post = Post.find(post_id)
+    post.update(title: @view.add_("NEW title of your post"))
+    post.update(url: @view.add_("NEW url of your post"))
+    p post
   end
 
   def destroy
     # DO NOT WRITE SQL QUERIES
     # TODO: destroy a post
+    post_id = @view.add_("ID of the post you want to DELETE.").to_i
+    post = Post.find(post_id)
+    post.destroy
   end
 
   def upvote
     # DO NOT WRITE SQL QUERIES
     # TODO: increment the `votes` column for a post
+    post_id = @view.add_("ID of the post you want to VOTE.").to_i
+    post = Post.find(post_id)
+    post.update(votes: post.votes + 1)
   end
 end
--- 03-AR-Database/03-ActiveRecord-Basics/03-Models/app/views/posts_view.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/03-Models/app/views/posts_view.rb
@@ -1,3 +1,14 @@
 class PostsView
   # TODO: implement some methods here when controller needs to `puts` or `gets`
+  def display_posts(posts)
+    posts.each_with_index do |post, index|
+      puts "#{index + 1}) ID: #{post.id} | #{post.title} | #{post.url} | VOTES
+      : #{post.votes}"
+    end
+  end
+
+  def add_(something)
+    puts "Please write the #{something}?"
+    gets.chomp
+  end
 end
