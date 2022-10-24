9dd881147011cf41375311613e62dd0007e470fd
Tash Cruz <sebastiantashika@gmail.com>
2022-10-24 15:55:13 +0900

    added 100 posts in DB

--- 03-AR-Database/03-ActiveRecord-Basics/04-Seed-Faker/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/04-Seed-Faker/db/seeds.rb
@@ -1,2 +1,11 @@
 require "faker"
+require_relative '../app/models/post'
 # TODO: Write a seed to insert 100 posts in the database
+100.times do |_i|
+  post = Post.new(
+    title: Faker::Coffee.variety,
+    url: Faker::Address.city,
+    votes: rand(1..100)
+  )
+  post.save!
+end
