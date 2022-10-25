7877b63dd2eff9cd2b042075fd26d3b2b4d7ac06
Tash Cruz <sebastiantashika@gmail.com>
2022-10-25 11:05:19 +0900

    some base code for the see from an API. Not working yet

--- 03-AR-Database/03-ActiveRecord-Basics/05-Seed-API/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/05-Seed-API/db/seeds.rb
@@ -2,3 +2,16 @@ require "json"
 require "rest-client"
 
 # TODO: Write a seed to insert 10 posts in the database fetched from the Hacker News API.
+url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+response = RestClient.get(url)
+repos = JSON.parse(response).first(10)
+repos.collect do |post|
+  p post
+end
+# 10.times do |p|
+#   post = Post.new(
+#     title: ,
+#     url: ,
+#     votes:
+#   )
+# end
