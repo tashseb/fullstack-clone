ae8e8c44d2529f85b7fd678aabc6a09895f3fe3e
Tash Cruz <sebastiantashika@gmail.com>
2022-10-25 12:14:15 +0900

    added validations and some callbacks

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
@@ -1,5 +1,11 @@
+require 'uri'
 class Post < ActiveRecord::Base
   belongs_to :user
-
   # TODO: Add some validation
+  validates :user, presence: true
+  validates :title, presence: true
+  validates :title, length: { minimum: 5 }
+  validates :title, uniqueness: { case_sensitive: false }
+  validates :url, presence: true
+  validates :url, format: { with: URI::DEFAULT_PARSER.make_regexp }
 end
--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
@@ -1,6 +1,22 @@
+# require 'fakemailer'
 class User < ActiveRecord::Base
   has_many :posts
-
   # TODO: Add some validation
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :email, presence: true
+  validates :email, format: { with: /\A.*@.*\.com\z/ }
   # TODO: Add some callbacks
+  before_validation :strip_email
+  after_create :welcome_email
+
+  private
+
+  def strip_email
+    self.email = email.strip unless email.nil?
+  end
+
+  def welcome_email
+    FakeMailer.instance.mail(email, "Welcome to HN!")
+  end
 end
