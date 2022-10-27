2b6e3c3c69d80855abbec0d71ecf543c0f7a7503
Tash Cruz <sebastiantashika@gmail.com>
2022-10-27 11:46:00 +0900

    saving all unsaved files from previous exercises.

--- 03-AR-Database/03-ActiveRecord-Basics/05-Seed-API/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/05-Seed-API/db/seeds.rb
@@ -1,12 +1,15 @@
 require "json"
 require "rest-client"
-
+require "open-uri"
+require "nokogiri"
 # TODO: Write a seed to insert 10 posts in the database fetched from the Hacker News API.
 url = "https://hacker-news.firebaseio.com/v0/topstories.json"
 response = RestClient.get(url)
 repos = JSON.parse(response).first(10)
-repos.collect do |post|
-  p post
+repos.each do |id|
+  html_content = URI.open("https://news.ycombinator.com/item?id=#{id}")
+  doc = Nokogiri::HTML(html_content)
+  info_hash = JSON.parse(doc.search()) 
 end
 # 10.times do |p|
 #   post = Post.new(
