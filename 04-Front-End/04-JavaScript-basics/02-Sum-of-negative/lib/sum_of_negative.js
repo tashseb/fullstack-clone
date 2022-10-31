6ec15ef15c28d3a15f79b8e57353b560cd0f45aa
Tash Cruz <sebastiantashika@gmail.com>
2022-10-31 09:30:50 +0900

    added a loop

--- 04-Front-End/04-JavaScript-basics/02-Sum-of-negative/lib/sum_of_negative.js
+++ 04-Front-End/04-JavaScript-basics/02-Sum-of-negative/lib/sum_of_negative.js
@@ -1,5 +1,10 @@
 const sumOfNegative = (numbers) => {
   // TODO: You are getting a `numbers` array. Return the sum of **negative** numbers only.
+  let result = 0;
+
+  numbers.forEach((num) => {
+    if (num < 0) result += num;
+  });
 };
 
 module.exports = sumOfNegative; // Do not remove.
