4be66772651b629847738ccd323a7e05c1f94d16
Tash Cruz <sebastiantashika@gmail.com>
2022-10-31 09:44:59 +0900

    Added a function to check validity of email

--- 04-Front-End/04-JavaScript-basics/03-Valid-email/lib/valid_email.js
+++ 04-Front-End/04-JavaScript-basics/03-Valid-email/lib/valid_email.js
@@ -1,5 +1,11 @@
 const valid = (email) => {
   // TODO: return true if the `email` string has the right pattern!
+  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if (email.match(regex)) {
+    return true;
+  }
+  return false;
 };
 
 module.exports = valid; // Do not remove.
