f7308dc0902698e2dd6bcc6d7e9f712931dfbdc7
Tash Cruz <sebastiantashika@gmail.com>
2022-10-31 10:34:52 +0900

    added codes in the arrow functions

--- 04-Front-End/04-JavaScript-basics/04-Unordered-list/lib/list_generator.js
+++ 04-Front-End/04-JavaScript-basics/04-Unordered-list/lib/list_generator.js
@@ -1,9 +1,17 @@
 const listItem = (content) => {
   // TODO: return the proper <li> HTML tag with its content (as a string)
+  return `<li class="list-group-item">${content}</li>`;
 };
 
 const unorderedList = (items) => {
   // TODO: return the proper <ul> markup (as a string)
+  let result = "";
+  result += '<ul class="list-group">';
+  items.forEach((item) => {
+    result += `<li class="list-group-item">${item}</li>`;
+  });
+  result += "</ul>";
+  return result;
 };
 
 module.exports = { listItem, unorderedList }; // Do not remove.
