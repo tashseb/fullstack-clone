f03a274739dfd0d5e71685038d6ad9fd68fbb867
Tash Cruz <sebastiantashika@gmail.com>
2022-11-02 08:50:46 +0900

    added some event listener but still not there yet

--- 04-Front-End/05-DOM-and-Events/04-Wagon-Race/lib/wagon_race.js
+++ 04-Front-End/05-DOM-and-Events/04-Wagon-Race/lib/wagon_race.js
@@ -1,1 +1,23 @@
 // TODO: write your code here
+document.addEventListener("DOMContentLoaded", function run () {
+  function keyPress() { (e) => {
+    if (e.charCode == "e"){
+        var tile = document.querySelector("#player1_strip > .active");  // get player tile
+        var nextTile = 'tile' + (tile.id[4] + 1) + 'e';  // get id of the next tile
+        tile.className = "";
+        document.getElementById(nextTile).className = "active";
+
+    }
+    else if (e.charCode == "d"){
+        var tile = document.querySelector("#player2_strip > .active");  // get player tile
+        var nextTile = 'tile' + (tile.id[4] + 1) + 'd';  // get id of the next tile
+        tile.className = "";
+        document.getElementById(nextTile).className = "active";
+
+    }
+    else{
+        alert("Invalid key stroke");
+    }
+}
+
+});
