915ceabbeac0a07a5672efbb36fc9d76fe04fdbe
Tash Cruz <sebastiantashika@gmail.com>
2022-11-01 15:55:08 +0900

    working code

--- 04-Front-End/05-DOM-and-Events/03-My-First-Event-Listener/lib/listener.js
+++ 04-Front-End/05-DOM-and-Events/03-My-First-Event-Listener/lib/listener.js
@@ -1,1 +1,6 @@
 // TODO: React to a click on the button!
+const button = document.getElementById("clickme");
+button.addEventListener("click", () => {
+  button.disabled = true;
+  button.innerHTML = "Bingo";
+});
