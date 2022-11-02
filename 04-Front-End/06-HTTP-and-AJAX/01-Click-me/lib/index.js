61ec77ce796e1819d7c59da7f4dac612c75aaa48
Tash Cruz <sebastiantashika@gmail.com>
2022-11-02 11:16:57 +0900

    added alert when button is clicked

--- 04-Front-End/06-HTTP-and-AJAX/01-Click-me/lib/index.js
+++ 04-Front-End/06-HTTP-and-AJAX/01-Click-me/lib/index.js
@@ -1,7 +1,11 @@
 const displayAlertOnButtonClick = () => {
   // TODO: Select the big green button
-
+  const button = document.querySelector(".btn");
   // TODO: Bind the `click` event to the button
+  button.addEventListener("click", (event) => {
+    console.log(event);
+    alert("Thank you!");
+  });
   // TODO: On click, display `Thank you!` in a JavaScript alert!
 };
 
