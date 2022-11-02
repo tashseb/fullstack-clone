c5261b84e1285e57bfdda48796fcca4122a8d582
Tash Cruz <sebastiantashika@gmail.com>
2022-11-02 11:16:19 +0900

    added alert when button is clicked

--- 04-Front-End/06-HTTP-and-AJAX/02-My-Favourite-Sports/lib/index.js
+++ 04-Front-End/06-HTTP-and-AJAX/02-My-Favourite-Sports/lib/index.js
@@ -1,1 +1,10 @@
 // TODO: write your code here!
+// Select an element
+const selected = document.querySelectorAll(".clickable");
+// Add an event listener when selected class is clicked
+selected.addEventListener("click", (event) => {
+  console.log(event);
+  selected.classList.add("active");
+  selected.classList.replace("clickable", "active");
+});
+// Change DOM by using CSS to show it is selected
