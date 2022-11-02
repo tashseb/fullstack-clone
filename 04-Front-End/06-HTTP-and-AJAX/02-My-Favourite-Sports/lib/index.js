347355287912117bb196cfde4616ff7d7a0782f1
Tash Cruz <sebastiantashika@gmail.com>
2022-11-02 11:38:53 +0900

    added toggle to switch css styles when clicked

--- 04-Front-End/06-HTTP-and-AJAX/02-My-Favourite-Sports/lib/index.js
+++ 04-Front-End/06-HTTP-and-AJAX/02-My-Favourite-Sports/lib/index.js
@@ -1,10 +1,12 @@
 // TODO: write your code here!
 // Select an element
-const selected = document.querySelectorAll(".clickable");
+const selection = document.querySelectorAll(".clickable");
 // Add an event listener when selected class is clicked
-selected.addEventListener("click", (event) => {
-  console.log(event);
-  selected.classList.add("active");
-  selected.classList.replace("clickable", "active");
+selection.forEach((selected) => {
+  selected.addEventListener("click", (event) => {
+    console.log(event);
+    // toggle functions changes it from original to the new class (clickable <-> active)
+    event.currentTarget.classList.toggle("active");
+  });
 });
 // Change DOM by using CSS to show it is selected
