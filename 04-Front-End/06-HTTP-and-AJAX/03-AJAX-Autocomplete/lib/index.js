782386613afa273cb23ffbdd0d3bff3c76f9ce4f
Tash Cruz <sebastiantashika@gmail.com>
2022-11-02 12:34:51 +0900

    added the auto-filling function

--- 04-Front-End/06-HTTP-and-AJAX/03-AJAX-Autocomplete/lib/index.js
+++ 04-Front-End/06-HTTP-and-AJAX/03-AJAX-Autocomplete/lib/index.js
@@ -1,1 +1,25 @@
 // TODO: Autocomplete the input with AJAX calls.
+const input = document.querySelector("#search");
+const list = document.querySelector("#results");
+
+input.addEventListener("keyup", (event) => {
+  console.log(event);
+  event.preventDefault();
+
+  const keyword = input.value;
+  const url = `https://wagon-dictionary.herokuapp.com/autocomplete/${keyword}`;
+  fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      const results = data.words;
+      list.innerHTML = "";
+      results.forEach((result) => {
+      // 3. Change the DOM (display the movies in the list)
+        list.insertAdjacentHTML(
+          "beforeend",
+          `<li class='list-inline-item'>${result}
+          </li>`
+        );
+      });
+    });
+});
