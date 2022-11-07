48fa9edf98b45a95bdd16176713da0cc90a44a92
Tash Cruz <sebastiantashika@gmail.com>
2022-11-07 11:05:47 +0900

    removed the useless assignment to make Rubo happy

--- 05-Rails/01-Rails-intro/01-HTTP-and-MVC-quiz/lib/quiz.rb
+++ 05-Rails/01-Rails-intro/01-HTTP-and-MVC-quiz/lib/quiz.rb
@@ -12,8 +12,8 @@ def http_requests_per_page
   # a web page, and the number of HTTP requests required to load that
   # page.
 
-  a = 'There is always one HTTP request per page.'
-  b = 'There is one HTTP request per resource/file needed for a web page (HTML, CSS, JS, etc).'
+  # a = 'There is always one HTTP request per page.'
+  # b = 'There is one HTTP request per resource/file needed for a web page (HTML, CSS, JS, etc).'
 
   return 'There is one HTTP request per resource/file needed for a web page (HTML, CSS, JS, etc).' # Return the correct variable, `a` or `b`
 end
@@ -21,9 +21,9 @@ end
 def view_responsibility
   # TODO: Return the variable whose value describes the responsibility of the
   # `View` in the MVC design pattern.
-  a = 'executes the appropriate reaction to user actions'
-  b = 'allows us to define data types in our application'
-  c = 'displays information to users'
+  # a = 'executes the appropriate reaction to user actions'
+  # b = 'allows us to define data types in our application'
+  # c = 'displays information to users'
 
   return 'displays information to users'  # Return the correct variable, `a`, `b` or `c`
 end
